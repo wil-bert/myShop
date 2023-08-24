f18ab3acc6a8edb26e1098bb0ea62469a7208f8e
wil-bert <ianwilbuts@gmail.com>
2023-08-24 21:08:14 +0300

    start

--- shop/src/app/app.component.spec.ts
+++ shop/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'duka'`, () => {
+  it(`should have as title 'shop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('duka');
+    expect(app.title).toEqual('shop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('duka app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('shop app is running!');
   });
 });
