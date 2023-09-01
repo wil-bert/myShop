ce5be3090cbdb43cfc0e1f045c983fb1999dcc51
wil-bert <ianwilbuts@gmail.com>
2023-09-01 13:16:45 +0300

    new component

--- shop/src/app/components/header/header.component.ts
+++ shop/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+
+
 }
+
+
